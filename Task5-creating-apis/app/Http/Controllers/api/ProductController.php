2fdd67fc4be770d6a0dd83da1ad79a4ba800d94d
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2023-10-28 04:20:47 +0200

    added return to store method in ProductController

--- Task5-creating-apis/app/Http/Controllers/api/ProductController.php
+++ Task5-creating-apis/app/Http/Controllers/api/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $product = Product::create($request->all());
-        return $product;
+        return response($product, 201);
     }
 
     /**
