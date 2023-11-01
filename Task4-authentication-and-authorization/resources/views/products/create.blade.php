2fd4dfd3300307cc98c9610928b5b7c29cfaa4cd
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2023-11-01 18:40:56 +0200

    used middleware

---
+++ Task4-authentication-and-authorization/resources/views/products/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+        <h1> Add new product</h1>
+
+        <form method="post" action="{{route('products.store')}}">
+                @csrf
+                <div class="mb-3">
+
+                        <div class="mb-3">
+                                <label class="form-label">Name</label>
+                                <input type="text" name="name" value="{{old('name')}}" class="form-control">
+                                @error('name')
+                                <div class=" text-danger fw-bold">{{ $message }}</div>
+                                @enderror
+                        </div>
+                        <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                                <input class="form-control" name="description" value="{{old('description')}}" id="exampleFormControlTextarea1" rows="3"></input>
+                                @error('description')
+                                <div class=" text-danger fw-bold">{{ $message }}</div>
+                                @enderror
+                        </div>
+                        <div class="mb-3">
+                                <label class="form-label">Image</label>
+                                <input type="text" name="image" value="{{old('image')}}" class="form-control">
+                                @error('image')
+                                <div class=" text-danger fw-bold">{{ $message }}</div>
+                                @enderror
+                        </div>
+                        <div class="mb-3">
+                                <label class="form-label">Price</label>
+                                <input type="text" name="price" value="{{old('price')}}" class="form-control">
+                                @error('price')
+                                <div class=" text-danger fw-bold">{{ $message }}</div>
+                                @enderror
+                        </div>
+                        <div class="mb-3">
+                                <label for="form-labe">Category</label>
+                                <select class="form-select" name="category_id" aria-label="Default select example">
+                                        <option selected value="">Open this select menu</option>
+                                        @foreach($categories as $category)
+                                        <option value="{{$category->id}}">{{$category->name}}</option>
+                                        @endforeach
+                                </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+</div>
+
+@endsection
