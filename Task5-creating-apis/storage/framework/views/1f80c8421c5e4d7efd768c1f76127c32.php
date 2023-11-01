3054ad2b4a03a2e213307b3ed822e558bd4cc3ce
Mohamed Gamal <76849157+Mohamed-G2021@users.noreply.github.com>
2023-11-01 03:24:09 +0200

    Delete Task5-creating-apis directory

--- Task5-creating-apis/storage/framework/views/1f80c8421c5e4d7efd768c1f76127c32.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /mnt/B082EAC082EA89E4/ITI ICC/22- Laravel/Labs/Task3-form-validation-and-using-resource-controller/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
